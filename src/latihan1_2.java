4bbb699f7dd57bcbe35c653fbdc6648bf4802aa5
Nadia Nur Saida <100014293+nadiianur@users.noreply.github.com>
2022-02-27 11:07:05 +0700

    Update latihan1_2.java

--- src/latihan1_2.java
+++ src/latihan1_2.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
+
 public class latihan1_2 {
     public static void main(String[] args) {
         String[] nama = { "a", "i", "d", "a" };
 
         // Perintah 2 : size()
 
-        System.out.println("\nUkuran array nama adalah " + nama.length);
+        ArrayList<String> nama1 = new ArrayList<String>();
+        for (int i = 0; i < nama.length; i++) {
+            nama1.add(nama[i]);
+        }
+
+        System.out.println("\nUkuran array nama adalah " + nama1.size());
     }
 }
